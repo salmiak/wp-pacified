b6b6b1869dd55bbb77e5f0fa74fa85d7fe5dfeef
Alfred Beckman (@salmiak) <alfred@salmiakmedia.se>
2016-04-04 20:35:07 +0200

    Front-page-scroll animation

--- components/front-page.php
+++ components/front-page.php
@@ -4,7 +4,7 @@
 <div id="MainContent">
   <div class="clipbox">
 
-    <div class="strip stripPrev">
+    <!--<div class="strip stripPrev">
       <img src="<?php bloginfo('template_url'); ?>/assets/img/mock1.png" class="stripImg" />
     </div>
 
@@ -14,7 +14,25 @@
 
     <div class="strip stripNext">
       <img src="<?php bloginfo('template_url'); ?>/assets/img/mock3.png" class="stripImg" />
-    </div>
+    </div>-->
+
+    <?php
+    $webcomics = new WP_Query(array(
+			'order' => 'ASC',
+			'post_type' => get_webcomic_collections(),
+			'posts_per_page' => 10
+		));
+
+    if ($webcomics and $webcomics->have_posts()) :
+			while ($webcomics->have_posts()) : $webcomics->the_post();
+        echo '<div class="strip">';
+        the_webcomic('large', 'self');
+        echo '</div>';
+			endwhile;
+		endif;
+
+		$webcomics->rewind_posts();
+    ?>
 
   </div>
 </div>
