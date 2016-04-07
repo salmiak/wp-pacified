5e9b5c46fc1325c238ca9a6623045b7455dbcce4
Alfred Beckman (@salmiak) <alfred@salmiakmedia.se>
2016-04-07 20:57:59 +0200

    Work on mobile version.

--- components/header.php
+++ components/header.php
@@ -11,6 +11,7 @@
 
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 
     <title><?php wp_title('&raquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
 
