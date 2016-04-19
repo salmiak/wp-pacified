67ac988f7a31f6b11a244b0e84f14b24d6f84427
Alfred Beckman (@salmiak) <alfred@salmiakmedia.se>
2016-04-19 15:19:17 +0200

    Improve mobile version. Made it possible to zoom. Made header not float. Added footer menu only shown on mobile.

--- components/footer.php
+++ components/footer.php
@@ -4,6 +4,10 @@
     <p> We &hearts; <a href="http://wordpress.org">Wordpress</a></p>
   </footer><!-- slut sidfot-->
 
+  <nav class="mobile-only">
+    <?php wp_nav_menu( 'sort_column=menu_order' ); ?>
+  </nav>
+
   <?php wp_footer(); ?>
 
 </body>
