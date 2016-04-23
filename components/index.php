f916f72783018ff73f972e1f873907f276eefd2f
Alfred Beckman (@salmiak) <alfred@salmiakmedia.se>
2016-04-23 20:30:27 +0200

    Added mail chimp signup form.

--- components/index.php
+++ components/index.php
@@ -22,6 +22,26 @@
 		$webcomics->rewind_posts();
     ?>
 
+    <div class="strip" id="strip_newsletter" data-storyslug="_newsletter" data-storyname="Newsletter" data-storydesc="Signup below!">
+
+      <!-- Begin MailChimp Signup Form -->
+      <link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
+      <div id="mc_embed_signup">
+      <form action="//pacifierscomic.us13.list-manage.com/subscribe/post?u=8da07976e650dd20b7a092da0&amp;id=a241bed3c0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+          <div id="mc_embed_signup_scroll">
+      	<label for="mce-EMAIL">Subscribe to our newsletter</label>
+      	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+          <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+          <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_8da07976e650dd20b7a092da0_a241bed3c0" tabindex="-1" value=""></div>
+          <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+          </div>
+      </form>
+      </div>
+
+      <!--End mc_embed_signup-->
+
+    </div>
+
     <?php if ( have_posts() && !is_home() ) { the_post(); ?>
     <script>
       pacified_start_id = 'strip<?php echo $post->ID; ?>';
